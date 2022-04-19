e187bf637f87b916d11a7658a9d1d74feabbff74
Yana <yana.ilina.1999@list.ru>
2022-04-20 00:09:01 +0300

    Open Question

---
+++ BLL/Questions/OpenQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Questions
+{
+    public class OpenQuestion: AbstractQuestion
+    {
+        public string Answer { get; set; }
+        public void WriteAnswer(string text)
+        {
+            Answer = text;
+        }
+    }
+}
