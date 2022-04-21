2e2f38a1367be8fa0060b3f6d78a7bc1c49a8f7a
Yana <yana.ilina.1999@list.ru>
2022-04-21 18:03:25 +0300

    Class Tests

---
+++ BLL/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    internal class Tests
+    {
+        public List<AbstractQuestion> Questions;
+
+
+        // редактирование вопроса в тесте
+        public List<AbstractQuestion> EditQuestion(List<AbstractQuestion> questions, int questionNumber)
+        {
+            questions[questionNumber] = new AbstractQuestion();
+            return questions;
+        }
+
+        // добавление нового вопроса в тест
+        public void AddQuestion(AbstractQuestion newQuestion)
+        {
+            Questions.Add(newQuestion);
+        }
+    }
+}
